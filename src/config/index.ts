d780afa5d1fb4bb39eb87e692fee94365e0d881c
Ritesh Singh <1124ritesh@gmail.com>
2022-05-15 20:03:21 +0530

    initial commit with base config
    Signed-off-by: Ritesh Singh <1124ritesh@gmail.com>

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+import { config } from "dotenv";
+config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+
+export const CREDENTIALS = process.env.CREDENTIALS === "true";
+export const {
+  NODE_ENV,
+  PORT,
+  MONGOOSE_URL,
+  SECRET_KEY,
+  LOG_FORMAT,
+  LOG_DIR,
+  ORIGIN,
+} = process.env;
